23f777a849125a44ec193f9d2e2c3da211e6a4a1
Kavindu8956 <sandeepa8956@gmail.com>
2020-03-21 21:49:06 +0530

    create user object on mainjs

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 'use strict';
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.urlencoded({extended: true}));
+
 app.use(express.static(__dirname));
 app.get('/', (req, res, next) => {
     res.sendFile('index.html');
 });
+
+var users = [];
+
+app.post('/users', function (req, res) {
+    const user = req.body;
+    user.birthday = new Date(user.birthday);
+    user.id = Date.now();
+    users.push(user);
+    res.json(user);
+});
+
 app.listen(3000, err => {
     if (err) {
         console.error(err);
